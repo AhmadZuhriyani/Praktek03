4b07cc4dc1059ed1518da5572c4dbeac5a9e6848
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:07:02 +0800

    Buat Getter and Setter

--- src/praktek03/Laptop.java
+++ src/praktek03/Laptop.java
@@ -9,7 +9,32 @@ public class Laptop {
     void cetakinfo(){
         System.out.println("Merk \t: "+merk+"\n"+
                           "Warna \t: "+warna+"\n"+
-                          "Harga \t: "+harga);
-       
+                          "Harga \t: "+harga);   
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+     
 }
