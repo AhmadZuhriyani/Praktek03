8699f470e8401e2eccd29479af7dc1c850097a07
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:22:04 +0800

    THROTTLE

--- src/praktek03/Komputer.java
+++ src/praktek03/Komputer.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class Komputer extends Laptop{
+    private String JenisKomputer;
+    
+    void throttle(){
+        System.out.println(" R.O.G -Gaming-");
+    }
     
 }
