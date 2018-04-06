afb0352595831c608d1c7cc022de9836d4d3b8df
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:16:47 +0800

    SUCCESSFUL USE - METHOD GET  :)

--- src/praktek03/LaptopAksi.java
+++ src/praktek03/LaptopAksi.java
@@ -8,5 +8,12 @@ public class LaptopAksi {
         R.setHarga(5000000);
         
         R.cetakinfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(R.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(R.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(R.getHarga());
     }
 }
