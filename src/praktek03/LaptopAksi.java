5a5567ed8794366f8c179b2ae0c7844d09cdb824
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:36:44 +0800

    LAST Step ( SUKSES ) Mantap Video nya

--- src/praktek03/LaptopAksi.java
+++ src/praktek03/LaptopAksi.java
@@ -15,5 +15,15 @@ public class LaptopAksi {
         System.out.println(R.getWarna());
         System.out.print("Harganya \t: ");
         System.out.println(R.getHarga());
+        
+        Komputer RZ = new Komputer();
+        RZ.setMerk("ASUS ROG");
+        RZ.setWarna("HITAM SILVER CASING");
+        RZ.setHarga(900000);
+        RZ.setJenisKomputer("ROG NEW");
+        
+        RZ.cetakinfo();
+        System.out.println("Jenis :"+RZ.getJenisKomputer());
+        RZ.throttle();
     }
-}
+} 
