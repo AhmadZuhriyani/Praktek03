e627bfd70d2a89bc86642b435f974512466d22a6
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 18:55:54 +0800

    Membuat CetakInfo

--- src/praktek03/Laptop.java
+++ src/praktek03/Laptop.java
@@ -5,4 +5,11 @@ public class Laptop {
     String merk;
     String warna;
     double harga;
+    
+    void cetakinfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                          "Warna \t: "+warna+"\n"+
+                          "Harga \t: "+harga);
+        
+    }
 }
