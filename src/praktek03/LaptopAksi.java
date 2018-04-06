e627ada83c26f1e5b475e9ffbaa9f8985fefa006
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:02:08 +0800

    Instance  (SUCCESSFUL) Panggil method + Buat Atribut

--- src/praktek03/LaptopAksi.java
+++ src/praktek03/LaptopAksi.java
@@ -1,4 +1,11 @@
 package praktek03;
 public class LaptopAksi {
-    
+    public static void main(String[] args) {
+        Laptop R = new Laptop();
+        R.merk="ASUS";
+        R.warna="HITAM";
+        R.harga=5000000;
+        
+        R.cetakinfo();
+    }
 }
