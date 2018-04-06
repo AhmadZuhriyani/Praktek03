ad50e45d7ec8237cf704cb335d787744aabddd3c
AhmadZuhriyani <izuhryan@gmail.com>
2018-04-06 19:10:16 +0800

    Menggunakan Metod SET

--- src/praktek03/Laptop.java
+++ src/praktek03/Laptop.java
@@ -36,5 +36,5 @@ public class Laptop {
         this.harga = harga;
     }
     
-     
+      
 }
--- src/praktek03/LaptopAksi.java
+++ src/praktek03/LaptopAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class LaptopAksi {
     public static void main(String[] args) {
         Laptop R = new Laptop();
-        R.merk="ASUS";
-        R.warna="HITAM";
-        R.harga=5000000;
+      
+        R.setMerk("ASUS");
+        R.setWarna("HITAM");
+        R.setHarga(5000000);
         
         R.cetakinfo();
     }
